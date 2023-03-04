3c74a915acfab3b788c5efe22ef7df85828b0c68
Saskle <31443808+Saskle@users.noreply.github.com>
2023-03-04 22:35:32 +0100

    Basic pellets added

---
+++ PacmanCloneUnity/PacmanClone/Assets/Scripts/Food.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public int points = 10;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        //add points
+        Destroy(gameObject);
+    }
+}
