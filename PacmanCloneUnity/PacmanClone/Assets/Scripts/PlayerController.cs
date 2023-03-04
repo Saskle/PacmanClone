f65f0fcc93b3308bfe0b32f8fcf945b46e77def0
Saskle <31443808+Saskle@users.noreply.github.com>
2023-03-04 21:56:34 +0100

    Basic playercontroller

---
+++ PacmanCloneUnity/PacmanClone/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 4f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+        transform.Translate(Vector3.up * Input.GetAxis("Vertical") * speed * Time.deltaTime);
+    }
+}
