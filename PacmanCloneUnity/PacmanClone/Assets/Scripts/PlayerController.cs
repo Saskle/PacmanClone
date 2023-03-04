841378aa6a55551e429e2691a4f9be7494903c5d
Saskle <31443808+Saskle@users.noreply.github.com>
2023-03-04 22:19:57 +0100

    Player respects collision now

--- PacmanCloneUnity/PacmanClone/Assets/Scripts/PlayerController.cs
+++ PacmanCloneUnity/PacmanClone/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 4f;
 
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,7 +24,12 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
-        transform.Translate(Vector3.up * Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+
+        //rb.AddForce(Vector2.up * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+        //rb.AddForce(Vector2.right * Input.GetAxis("Vertical") * speed * Time.deltaTime);
+
+        //transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+        //transform.Translate(Vector3.up * Input.GetAxis("Vertical") * speed * Time.deltaTime);
     }
 }
