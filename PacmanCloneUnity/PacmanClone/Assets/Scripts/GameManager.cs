e07271b6178f78d269906e5e41bdb4b711a0e519
Saskle <31443808+Saskle@users.noreply.github.com>
2023-03-05 21:39:06 +0100

    Spawn pellets at start of the game

--- PacmanCloneUnity/PacmanClone/Assets/Scripts/GameManager.cs
+++ PacmanCloneUnity/PacmanClone/Assets/Scripts/GameManager.cs
@@ -1,15 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject pelletPrefab;
+    [SerializeField] private Tilemap tileMap;
+    [SerializeField] private List<Vector3> spawnPositions;
+
     public int currentScore { get; private set; }
-
-
     public static GameManager Instance { get; private set; }
 
     private void Awake()
     {
-        // singleton implementation
+        // Singleton implementation
         if (Instance != null && Instance != this)
         {
             Destroy(this);
@@ -26,12 +30,41 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // spawn pellets
+        SpawnPellets();
     }
 
     public void AddPoints(int points)
     {
         currentScore += points;
         Debug.Log("Score is " + currentScore);
+    }
+
+    private void SpawnPellets()
+    {
+        spawnPositions = new List<Vector3>();
+
+        for (int n = tileMap.cellBounds.xMin; n < tileMap.cellBounds.xMax; n++) // scan the tilemap from left to right for tiles
+        {
+            for (int p = tileMap.cellBounds.yMin; p < tileMap.cellBounds.yMax; p++) // scan from bottom to top for tiles
+            {
+                // find local position of current tile on the tilemap and convert this to world coordinates
+                Vector3Int localPlace = new Vector3Int(n, p, (int)tileMap.transform.position.y);
+                Vector3 place = tileMap.CellToWorld(localPlace);
+                
+                // if there is a tile at localPlace, add it to the list
+                if (tileMap.HasTile(localPlace))
+                {
+                    spawnPositions.Add(place);
+                }
+
+            }
+        }
+
+        for (int i = 0; i < spawnPositions.Count; i++)
+        {
+            // spawn prefab at found tiles with half of tile size's offset (0.35/2 = 0.175)
+            Instantiate(pelletPrefab, new Vector3(spawnPositions[i].x + 0.175f, spawnPositions[i].y + 0.175f, spawnPositions[i].z), Quaternion.identity);
+        }
+
     }
 }
