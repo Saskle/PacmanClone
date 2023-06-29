e9e88887947b5b1862065da85fb0969f872cf20e
Saskle <31443808+Saskle@users.noreply.github.com>
2023-06-29 21:12:50 +0200

    Made the teleport tunnel (works only for PacMan yet)

---
+++ PacmanCloneUnity/PacmanClone/Assets/Scripts/Teleport.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField] private GameObject otherTeleport;
+    [SerializeField] private bool isRightTeleport;
+    private Vector3 offset = new Vector3(1, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Ghost"))
+        {
+            if (isRightTeleport)
+            {
+                collision.gameObject.transform.position = otherTeleport.transform.position + offset;
+            }
+            else
+            {
+                collision.gameObject.transform.position = otherTeleport.transform.position - offset;
+            }
+            
+        }
+    }
+}
